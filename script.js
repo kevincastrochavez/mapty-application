2fa1388dac65242e968fa937266d2a892a8b52d1
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-01-26 09:59:46 -0600

    learned how to use the geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+  },
+  function () {
+    alert('Could not get yout position');
+  }
+);
